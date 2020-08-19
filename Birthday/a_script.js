f285649ffed29d83b66bf929fe4165b0c8793bd5
hithereryan <45579106+hithereryan@users.noreply.github.com>
2020-08-19 13:45:23 -0700

    Update a_script.js

--- Birthday/a_script.js
+++ Birthday/a_script.js
@@ -67,6 +67,7 @@ for (var i = 0; i<slides.length; i++){
     document.getElementById("name").innerHTML += this.getBoundingClientRect().width + ", " + this.width + ", ";
     if (s==slides.length-1){
       changeSlide();
+      document.getElementById("message").innerHTML = this.offsetWidth + ", " + this.width;
     }
   }
   tempSlide.src = slides[i];
